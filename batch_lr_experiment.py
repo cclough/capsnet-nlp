db6c617867c19bae16d4e37b0a031dd6ce6f6164
Christian Clough <christian@cclough-mba.local>
2018-04-08 20:42:53 -0700

    boom

--- batch_lr_experiment.py
+++ batch_lr_experiment.py
@@ -34,8 +34,8 @@ top_words = 5000
 (X_train, y_train), (X_test, y_test) = imdb.load_data()#num_words=top_words)
 # pad dataset to a maximum review length in words
 max_words = 500
-X_train = sequence.pad_sequences(X_train, maxlen=max_words)
-X_test = sequence.pad_sequences(X_test, maxlen=max_words)
+X_train = sequence.pad_sequences(X_train)#, maxlen=max_words)
+X_test = sequence.pad_sequences(X_test)#, maxlen=max_words)
 
 
 
